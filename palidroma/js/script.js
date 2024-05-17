7631d7ee17b5112553fcaccfcd76caa61a8c7674
martinaliporata <martinaliporata@gmail.com>
2024-05-17 07:53:51 +0200

    aggiunto prompt

---
+++ palidroma/js/script.js
@@ -0,0 +1,8 @@
+// Palidroma
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+
+// Con un prompt chiediamo all’utente di inserire una parola
+// Una parola è palindroma se, letta al contrario, rimane invariata. 
+
+const userWord = prompt('Inserisci una parola')
